e7ebc12580bc7b7a18ce6151d3916c406f429ee4
Ah <lnl90333@gmail.com>
2022-03-05 14:48:15 +0800

    实现绘制ImGui菜单 存在bug: 菜单无法移动

--- Cheat/Menu.cpp
+++ Cheat/Menu.cpp
@@ -1,115 +1,56 @@
 #include "pch.h"
 #include "framework.h"
 
+void drawRect::GetRect()
+{
+	const std::shared_ptr<RECT> rect = std::make_shared<RECT>();
+	GetWindowRect(Menu::get()->game_hwnd, rect.get());
+
+	x = rect->left;
+	y = rect->top;
+
+	width = rect->right - rect->left;
+	height = rect->bottom - rect->top;
+}
+
 void Menu::Execute()
 {
 	std::thread(&Menu::CreateMenu, this).detach();
 }
 
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT Menu::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
 	switch (msg)
 	{
-	case WM_PAINT:
-
-		break;
-	case WM_CREATE:
+	case WM_SIZE:
+		if (get()->g_pd3dDevice != nullptr && wParam != SIZE_MINIMIZED)
+		{
+			if (get()->g_mainRenderTargetView) { get()->g_mainRenderTargetView->Release(); get()->g_mainRenderTargetView = nullptr; }
+			get()->g_pSwapChain->ResizeBuffers(0, LOWORD(lParam), HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+			ID3D11Texture2D* pBackBuffer;
+			get()->g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+			get()->g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &get()->g_mainRenderTargetView);
+			pBackBuffer->Release();
+		}
+		return false;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return false;
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
-		break;
+		return false;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-//-----------------------------------------------------------------
-// Data
-static ID3D11Device* g_pd3dDevice = NULL;
-static ID3D11DeviceContext* g_pd3dDeviceContext = NULL;
-static IDXGISwapChain* g_pSwapChain = NULL;
-static ID3D11RenderTargetView* g_mainRenderTargetView = NULL;
-
-void CreateRenderTarget()
-{
-	ID3D11Texture2D* pBackBuffer;
-	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
-	pBackBuffer->Release();
-}
-
-bool CreateDeviceD3D(HWND hWnd)
-{
-	// Setup swap chain
-	DXGI_SWAP_CHAIN_DESC sd;
-	ZeroMemory(&sd, sizeof(sd));
-	sd.BufferCount = 2;
-	sd.BufferDesc.Width = 0;
-	sd.BufferDesc.Height = 0;
-	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 60;
-	sd.BufferDesc.RefreshRate.Denominator = 1;
-	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	sd.OutputWindow = hWnd;
-	sd.SampleDesc.Count = 1;
-	sd.SampleDesc.Quality = 0;
-	sd.Windowed = TRUE;
-	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-
-	UINT createDeviceFlags = 0;
-	//createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-	D3D_FEATURE_LEVEL featureLevel;
-	const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
-	if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
-		return false;
-
-	CreateRenderTarget();
-	return true;
-}
-
-void CleanupRenderTarget()
-{
-	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
-}
-
-void CleanupDeviceD3D()
-{
-	CleanupRenderTarget();
-	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
-	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
-	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
-}
-//-----------------------------------------------------------------
-
 void Menu::CreateMenu()
 {
-	WNDCLASSEX wc
-	{
-		sizeof(WNDCLASSEX),
-		CS_VREDRAW | CS_HREDRAW | CS_CLASSDC,
-		WndProc,
-		0L,0L,GetModuleHandle(nullptr),
-		nullptr,nullptr,nullptr,nullptr, " ",nullptr
-	};
-
-	RegisterClassEx(&wc);
-
-	//获得大小
-	RECT rect;
-	if (!GetWindowRect(gmae_hwnd, &rect)) {
-		MessageBox(nullptr, "游戏未开启", "提示", MB_OK);
-		ExitProcess(1);
-	}
-
-	//打开窗口
-	hWindow = CreateWindowEx(
-		WS_EX_TRANSPARENT | WS_EX_LAYERED,
-		wc.lpszClassName, " ", WS_POPUP,
-		rect.left, rect.top,
-		rect.right - rect.left,
-		rect.bottom - rect.top,
-		nullptr, nullptr, wc.hInstance, &wc
-	);
+	//窗口初始化
+	WindowInit();
 
 	//初始化D3D
 	if (!CreateDeviceD3D(hWindow))
@@ -127,15 +68,7 @@ void Menu::CreateMenu()
 	UpdateWindow(hWindow);
 
 	//ImGui初始化
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.WantSaveIniSettings = false;
-	io.IniFilename = nullptr;
-
-	ImGui::StyleColorsLight();
-	ImGui_ImplWin32_Init(hWindow);
-	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+	ImGuiInit();
 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(MSG));
@@ -146,6 +79,21 @@ void Menu::CreateMenu()
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+		rect.GetRect();
+		if (GetForegroundWindow() == game_hwnd)
+		{
+			SetWindowPos(hWindow, HWND_TOPMOST,
+				rect.x, rect.y, rect.width, rect.height,
+				SWP_SHOWWINDOW);
+		}
+		else
+		{
+			SetWindowPos(hWindow, HWND_BOTTOM,
+				rect.x, rect.y, rect.width, rect.height,
+				SWP_SHOWWINDOW);
+		}
+		MoveWindow(hWindow, rect.x,rect.y, rect.width,rect.height, true);
+
 		ImGui_ImplDX11_NewFrame();
 		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
@@ -167,4 +115,89 @@ void Menu::CreateMenu()
 		g_pSwapChain->Present(1, 0);
 	}
 
+}
+
+void Menu::WindowInit()
+{
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetModuleHandle(nullptr);
+	wc.hIcon = nullptr;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = " ";
+	wc.lpszClassName = " ";
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	//获得大小
+	rect.GetRect();
+
+	//打开窗口
+	hWindow = CreateWindowEx(
+		WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+		wc.lpszClassName, " ", 
+		WS_POPUP | WS_VISIBLE,
+		rect.x, rect.y,rect.width,rect.height,
+		nullptr, nullptr, wc.hInstance, &wc
+	);
+}
+
+void Menu::ImGuiInit() const
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.WantSaveIniSettings = false;
+	io.IniFilename = nullptr;
+
+	ImGui::StyleColorsLight();
+	ImGui_ImplWin32_Init(hWindow);
+	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+}
+
+bool Menu::CreateDeviceD3D(HWND hWnd)
+{
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 2;
+	sd.BufferDesc.Width = 0;
+	sd.BufferDesc.Height = 0;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	D3D_FEATURE_LEVEL featureLevel;
+	constexpr D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+	if (D3D11CreateDeviceAndSwapChain(
+		nullptr, D3D_DRIVER_TYPE_HARDWARE, 
+		nullptr, 0, featureLevelArray, 
+		2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, 
+		&featureLevel, &g_pd3dDeviceContext) != S_OK
+		)return false;
+
+	ID3D11Texture2D* pBackBuffer;
+	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+	pBackBuffer->Release();
+	return true;
+}
+void Menu::CleanupDeviceD3D()
+{
+	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
 }
--- Cheat/Menu.h
+++ Cheat/Menu.h
@@ -1,18 +1,47 @@
 #pragma once
 
+class drawRect
+{
+public:
+	int x, y, width, height;
+
+	void GetRect();
+};
+
 class Menu : public Singleton<Menu>
 {
 public:
 	void Execute();
+
+	//D3D11
+	ID3D11Device* g_pd3dDevice = nullptr;
+	ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+	IDXGISwapChain* g_pSwapChain = nullptr;
+	ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+
+	//游戏HWND
+	HWND game_hwnd = GetCurrentProcessH();
 private:
 	static LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 	//打开菜单
 	void CreateMenu();
 
+	//窗口初始化
+	void WindowInit();
+
+	//ImGui初始化
+	void ImGuiInit() const;
+
+	//--------D3D--------
+	bool CreateDeviceD3D(HWND hWnd);//打开设备 
+	void CleanupDeviceD3D();//释放设备
+	//-------------------
+
+	WNDCLASSEX wc;
+
+	drawRect rect;
+
 	//窗口HWND
 	HWND hWindow;
-
-	//游戏HWND
-	HWND gmae_hwnd = FindWindow(nullptr, "PvZ-Eagrace Max By Henry-joestar   (B&Eteam)  QQgroup:821960155");
 };
