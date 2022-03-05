e4458e2c1b77a11197d37bf8e07150d3ee6996b7
Ah <lnl90333@gmail.com>
2022-03-05 11:31:48 +0800

    初始化

---
+++ Cheat/dllmain.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "framework.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        break;
+    case DLL_PROCESS_DETACH:
+
+        break;
+    }
+    return TRUE;
+}
+
